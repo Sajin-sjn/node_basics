3e4c7c113653747d36512b11afaea1ae7a370aae
Sajin PJ <sajinsjn4433@gmail.com>
2024-09-11 12:22:00 +0530

    node pure basic to create a server, request to server and response, level 1

---
+++ sampleServer.js
@@ -0,0 +1,7 @@
+// down to basics, to create a node server also to display a string
+// var http = require('http');
+
+// http.createServer((req,res) => {
+//     res.write('helllo world')
+//     res.end()
+// }).listen(8800)
