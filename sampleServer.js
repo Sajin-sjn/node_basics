25e3fc8e4c3164374690911ac89c8eac733057fd
Sajin PJ <sajinsjn4433@gmail.com>
2024-09-11 12:23:05 +0530

    level 2 server creation, renovated with status code

--- sampleServer.js
+++ sampleServer.js
@@ -5,3 +5,14 @@
 //     res.write('helllo world')
 //     res.end()
 // }).listen(8800)
+
+
+// advanced version that is over the top by including the status code.
+
+// var http = require('http')
+
+// http.createServer((req,res) => {
+//     res.writeHead(200,{'content-type':'text/html'})
+//     res.write("hello world")
+//     res.end()
+// }).listen(2500)
